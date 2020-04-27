edc1d6ce0c78acf9c6e474ab060c2315473d83b8
bear2529 <bear_29@163.com>
2020-04-27 14:55:40 +0800

    first commit
    add files and folder

---
+++ MDK-ARM/RTE/_usb_vpc_cubemx/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usb_vpc_cubemx' 
+ * Target:  'usb_vpc_cubemx' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/usb_vpc_cubemx/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+usb_vpc_cubemx\startup_stm32f407xx.o: startup_stm32f407xx.s
